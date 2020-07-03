b6e36e5561afe85e3bcf6dddfec9ab36ff6757be
Rakesh Chavan <Rakesh>
2020-07-03 12:03:54 +0530

    first commit

---
+++ guitarApp/admin.py
@@ -0,0 +1,12 @@
+
+
+# Register your models here.
+from django.contrib import admin
+from guitarApp.models import *
+
+admin.site.register(YoutubeVideos)
+admin.site.register(AboutGuitar)
+admin.site.register(Batches)
+admin.site.register(Feestructure)
+admin.site.register(Buyguitar)
+admin.site.register(Post)
